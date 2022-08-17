b642746373a935922ad2d867629ba8b7e8d7ad10
hxyro <hxyro0@gmail.com>
2022-08-17 17:23:40 +0530

    data source

--- ec2-node.tf
+++ ec2-node.tf
@@ -1,32 +1,43 @@
-# data "aws_ami" "amazon_linux" {
-#   most_recent = true
+data "aws_ami" "amazon_linux" {
+  most_recent = true
 
-#   filter {
-#     name   = "name"
-#     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-#   }
-#   filter {
-#         name   = "virtualization-type"
-#         values = ["hvm"]
-#     }
-#   # owners = [""]
-# }
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0*"]
+  }
 
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  owners = ["amazon"]
+}
 
 resource "aws_instance" "web-server" {
-  ami               = "ami-068257025f72f470d"
+  ami               = data.aws_ami.amazon_linux.id
   instance_type     = var.ec2-type
   availability_zone = var.availability_zone
-  key_name          = "terra"
+  key_name          = var.key
 
 
   network_interface {
     device_index         = 0
     network_interface_id = aws_network_interface.web_server.id
   }
-  # user_data = file("Open.sh")
+  user_data = file("Openvpn.sh")
 
   tags = {
     Name = var.ec2-name
   }
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.web-server.public_ip
 }
